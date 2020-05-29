f430b8cb9611066d5cda078da54aedd8827b4cc3
kaztom <kazuhito1oo1@gmail.com>
2020-05-29 13:57:29 +0900

    S3が使用できるように

--- src/config/filesystems.php
+++ src/config/filesystems.php
@@ -57,6 +57,8 @@ return [
 
         's3' => [
             'driver' => 's3',
+            'endpoint' => env('AWS_URL'),
+            'use_path_style_endpoint' => env('AWS_PATH_STYLE_ENDPOINT', false),
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
